b45fa5788063b3abe342191a9763808f0ba0e054
Jair <jairmsn@hotmail.com>
2019-09-01 18:11:14 -0300

    add leitura de base e criação do arquivo de saída

--- Program.cs
+++ Program.cs
@@ -12,17 +12,17 @@ namespace empilhador
         {
 
             int linha = 2;
-            int coluna = 6; 
+            int coluna = 7;
             string pathArquivoConfig = Directory.GetCurrentDirectory() + @"\Arquivos\Config.xlsx";
             string pathSaida = Directory.GetCurrentDirectory() + @"\Arquivos\Saida.xlsx";
             var fi = new FileInfo(pathArquivoConfig);
-            
+            var cf = new Config();
+
             // Ler o arquivo Config
             using (var p = new ExcelPackage(fi))
             {
-                
+
                 var ws = p.Workbook.Worksheets["Config"];
-                var cf = new Config();
 
                 // Armazena os nomes de campos em listas
                 while (!(ws.Cells[1, coluna].Value == null))
@@ -34,19 +34,20 @@ namespace empilhador
                 // Armazena os parâmetros em listas
                 while (!(ws.Cells[linha, 1].Value == null))
                 {
-                    coluna = 6;
+                    coluna = 7;
                     cf.LNomeBase.Add(ws.Cells[linha, 1].Value.ToString());
-                    cf.LPathBase.Add(ws.Cells[linha, 2].Value.ToString());
-                    cf.LLinhaIN.Add(Int32.Parse(ws.Cells[linha, 3].Value.ToString()));
-                    cf.LColunaIN.Add(Int32.Parse(ws.Cells[linha, 4].Value.ToString()));
-                    cf.LColunaKey.Add(Int32.Parse(ws.Cells[linha, 5].Value.ToString()));
-                    
-                    cf.LParametros.Add(new List<string>());
-                    
-                    for (int i = 0; i<cf.LNomeCampo.Count; i++)
+                    cf.LSheet.Add(ws.Cells[linha, 2].Value.ToString());
+                    cf.LPathBase.Add(ws.Cells[linha, 3].Value.ToString());
+                    cf.LLinhaIN.Add(Int32.Parse(ws.Cells[linha, 4].Value.ToString()));
+                    cf.LColunaIN.Add(Int32.Parse(ws.Cells[linha, 5].Value.ToString()));
+                    cf.LColunaKey.Add(Int32.Parse(ws.Cells[linha, 6].Value.ToString()));
+
+                    cf.LParametros.Add(new List<int>());
+
+                    for (int i = 0; i < cf.LNomeCampo.Count; i++)
                     {
                         if (ws.Cells[linha, coluna].Value == null) break;
-                        cf.LParametros[cf.LParametros.Count - 1].Add(ws.Cells[linha, coluna].Value.ToString());
+                        cf.LParametros[cf.LParametros.Count - 1].Add(Int32.Parse(ws.Cells[linha, coluna].Value.ToString()));
                         coluna++;
                     }
 
@@ -58,17 +59,58 @@ namespace empilhador
             //Gera Arquivo de saida
             using (var p = new ExcelPackage())
             {
+                int colunaSaida = 1;
+                int linhaSaida = 2;
 
-                foreach (var item in )
+                List<int> sublista = null;
+
+                var wssaida = p.Workbook.Worksheets.Add("Saida");
+                
+                //Escreve o nome das colunas no arquivo de saída
+                foreach (var item in cf.LNomeCampo)
                 {
-                    
+                    wssaida.Cells[1, colunaSaida].Value = item.ToString();
+                    colunaSaida++;
                 }
 
-                var ws=p.Workbook.Worksheets.Add("Saida");
-                ws.Cells["A1"].Value = "This is cell A1";
+                for (int i = 0; i < cf.LNomeBase.Count; i++)
+                {
+                    // Lê as bases
+                    using (var b = new ExcelPackage(new FileInfo(cf.LPathBase[i])))
+                    {
+
+                        var wsbase = b.Workbook.Worksheets[cf.LSheet[i]];
+                        linha = cf.LLinhaIN[i];
+                        coluna = cf.LColunaKey[i];
+
+
+                        while (!(wsbase.Cells[linha, coluna].Value == null))
+                        {
+                            for (int j = 0; j < cf.LParametros.Count; j++)
+                            {
+                                wssaida.Cells[linhaSaida, 1].Value = cf.LNomeBase[i];
+                                sublista = cf.LParametros[i];
+                                wssaida.Cells[linhaSaida, j + 1].Value = wsbase.Cells[linha, sublista[j]].Value;
+                            }
+
+                            linha++;
+                            linhaSaida++;
+                        }
+
+                    }
+
+                }
+
                 p.SaveAs(new FileInfo(pathSaida));
             }
-        
+
+
+
+
         }
+
+
     }
+
 }
+
